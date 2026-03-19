543ba21946d9f4fc8bae5c061cc0bd7c551759cf
darmungandr-coder <darmungandr@gmail.com>
2026-03-19 23:21:41 +0100

    finishing test

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -12,7 +12,7 @@ test('Test Case 1: Register User', async ({ page }) => {
     await nameInput.fill('NewUser');
     await emailInput.fill('NewUser@email.com')
     await signupButton.click();
-    await expect(page.getByRole('heading', { name: 'Enter Account I nformation' })).toBeVisible();
+    await expect(page.getByRole('heading', { name: 'Enter Account Information' })).toBeVisible();
     await page.getByRole('radio', {name: 'Mr.'}).check()
     await expect(page.getByRole('radio', { name: 'Mr.' })).toBeChecked();
     await expect(page.getByRole('radio', { name: 'Mrs.' })).not.toBeChecked();
@@ -25,9 +25,21 @@ test('Test Case 1: Register User', async ({ page }) => {
     await expect(page.getByRole('checkbox', { name: 'Sign up for our newsletter!' })).toBeChecked();
     await page.getByRole('checkbox', {name: 'Receive special offers from our partners!'}).check()
     await expect(page.getByRole('checkbox', {name: 'Receive special offers from our partners!'})).toBeChecked()
-    
-
-    await page.pause();
-    
-    
+    await page.getByRole('textbox', { name: 'First name *' }).fill('Daniil')
+    await page.getByRole('textbox', { name: 'Last name *' }).fill('Gladkiy')
+    await page.locator('#company').fill('FLS')
+    await page.locator('#address1').fill('Zizkov')
+    await page.locator('#address2').fill('Praha')
+    await page.locator('#country').selectOption('Canada')
+    await page.locator('#state[data-qa="state"]').fill('Illinois')
+    await page.locator('#city[data-qa="city"]').fill('Praha')
+    await page.locator('#zipcode[data-qa="zipcode"]').fill('60654')
+    await page.locator('#mobile_number[data-qa="mobile_number"]').fill('707543')
+    await page.getByRole('button', { name: 'Create Account' }).click()
+    await expect(page.locator('b:has-text("ACCOUNT CREATED!")')).toHaveText('Account Created!');
+    await page.locator('a:has-text("Continue")').click()
+    await expect(page.getByRole('link', { name: 'Delete Account' })).toBeVisible()
+    await page.getByRole('link', { name: 'Delete Account' }).click()
+    await expect(page.locator('b:has-text("ACCOUNT DELETED!")')).toBeVisible()
+    await expect(page.locator('b:has-text("ACCOUNT DELETED!")')).toHaveText('Account Deleted!');
 })
