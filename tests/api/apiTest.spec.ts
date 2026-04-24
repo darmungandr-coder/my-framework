7f115def4d73800c0b4c9e376236aae40fb70015
Daniel <darmungandr@gmail.com>
2026-04-24 18:54:21 +0200

    Refactoring of api tests

--- tests/api/apiTest.spec.ts
+++ tests/api/apiTest.spec.ts
@@ -1,28 +1,31 @@
     import { test, expect } from '@playwright/test';
 
     test('Get all products', async ({request}) =>{
-        const response = await request.get('https://automationexercise.com/api/productsList')
+        const response = await request.get('/api/productsList')
         expect(response.status()).toBe(200)
         const body = await response.json()
         expect(Array.isArray(body.products)).toBeTruthy()
         expect(body.products.length).toBeGreaterThan(0)
         for(const product of body.products){
-            expect(product).toHaveProperty('id')
-            expect(product).toHaveProperty('name')
-            expect(product).toHaveProperty('price')
-            expect(product).toHaveProperty('brand')
-            expect(product).toHaveProperty('category')
-            expect(product.category).toHaveProperty('usertype')
-
+            expect(product).toEqual(expect.objectContaining({
+                id: expect.any(Number),
+                name: expect.any(String),
+                price: expect.any(String),
+                brand: expect.any(String),
+                category: expect.any(Object),
+            }))
+            expect(product.category).toEqual(expect.objectContaining({
+                usertype: expect.any(Object),
+                category: expect.any(String),
+            }))
         }
 })
-
     test('Post to All Products List', async ({request}) => {
-        const response = await request.post('https://automationexercise.com/api/productsList')
+        const response = await request.post('/api/productsList')
         expect(response.status()).toBe(200)
-        const body = await response.json()        
-        expect(body).toHaveProperty('responseCode')
-        expect(body.responseCode).toBe(405)
-        expect(body).toHaveProperty('message')
-        expect(body.message).toBe('This request method is not supported.')
+        const body = await response.json()
+        expect(body).toEqual(expect.objectContaining({
+            responseCode: expect.any(Number),
+            message: expect.any(String)
+        }))
         })
